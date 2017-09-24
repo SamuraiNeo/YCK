8477e032fe3dac94df456937abf6e97a4d0f98fd
SamuraiNeo <corpo369@onet.pl>
2017-09-24 18:42:04 +0200

    Data - Periods

--- data.py
+++ data.py
@@ -1,10 +1,8 @@
-pertab = [group1, group2, group3, group4, group5, group6, group7, group8, group9,
-          group10, group11, group12, group13, group14, group15, group16, group17, group18]
 group1 = ["H", "Li", "Na", "K", "Rb", "Cs", "Fr"]
 group2 = [" ", "Be", "Mg", "Ca", "Sr", "Ba", "Ra"]
-group3 = [" ", " ", " ", "Sc", "Y", lanthanoids, actinoids]
 lanthanoids = ["La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu"]
 actinoids = ["Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr"]
+group3 = [" ", " ", " ", "Sc", "Y", lanthanoids, actinoids]
 group4 = [" ", " ", " ", "Ti", "Zr", "Hf", "Rf"]
 group5 = [" ", " ", " ", "V", "Nb", "Ta", "Db"]
 group6 = [" ", " ", " ", "Cr", "Mo", "W", "Sg"]
@@ -20,3 +18,26 @@ group15 = [" ", "N", "P", "As", "Sb", "Bi", "Mc"]
 group16 = [" ", "O", "S", "Se", "Te", "Po", "Lv"]
 group17 = [" ", "F", "Cl", "Br", "I", "At", "Ts"]
 group18 = ["He", "Ne", "Ar", "Kr", "Xe", "Rn", "Og"]
+period1 = [group1[0], group2[0], group3[0], group4[0], group5[0], group6[0],
+           group7[0], group8[0], group9[0], group10[0], group11[0], group12[0],
+           group13[0], group14[0], group15[0], group16[0], group17[0], group18[0]]
+period2 = [group1[1], group2[1], group3[1], group4[1], group5[1], group6[1],
+           group7[1], group8[1], group9[1], group10[1], group11[1], group12[1],
+           group13[1], group14[1], group15[1], group16[1], group17[1], group18[1]]
+period3 = [group1[2], group2[2], group3[2], group4[2], group5[2], group6[2],
+           group7[2], group8[2], group9[2], group10[2], group11[2], group12[2],
+           group13[2], group14[2], group15[2], group16[2], group17[2], group18[2]]
+period4 = [group1[3], group2[3], group3[3], group4[3], group5[3], group6[3],
+           group7[3], group8[3], group9[3], group10[3], group11[3], group12[3],
+           group13[3], group14[3], group15[3], group16[3], group17[3], group18[3]]
+period5 = [group1[4], group2[4], group3[4], group4[4], group5[4], group6[4],
+           group7[4], group8[4], group9[4], group10[4], group11[4], group12[4],
+           group13[4], group14[4], group15[4], group16[4], group17[4], group18[4]]
+period6 = [group1[5], group2[5], "LX", group4[5], group5[5], group6[5],
+           group7[5], group8[5], group9[5], group10[5], group11[5], group12[5],
+           group13[5], group14[5], group15[5], group16[5], group17[5], group18[5]]
+period7 = [group1[6], group2[6], "AX", group4[6], group5[6], group6[6],
+           group7[6], group8[6], group9[6], group10[6], group11[6], group12[6],
+           group13[6], group14[6], group15[6], group16[6], group17[6], group18[6]]
+pertab = [group1, group2, group3, group4, group5, group6, group7, group8, group9,
+          group10, group11, group12, group13, group14, group15, group16, group17, group18]
