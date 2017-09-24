cb760d1472b25242c41ea1a2229e49ea1bac4a8b
SamuraiNeo <corpo369@onet.pl>
2017-09-24 17:39:34 +0200

    Main file

---
+++ chem.py
@@ -0,0 +1,7 @@
+pertab = [group1, group2, group3, group4, group5, group6, group7, group8, group9,
+          group10, group11, group12, group13, group14, group15, group16, group17, group18]
+group1 = ["H", "Li", "Na", "K", "Rb", "Cs", "Fr"]
+group2 = [" ", "Be", "Mg", "Ca", "Sr", "Ba", "Ra"]
+group3 = [" ", " ", " ", "Sc", "Y", lanthanoids, actinoids]
+lanthanoids = ["La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu"]
+actinoids = ["Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr"]
